8b443fee23802e180edc98356a23c112dc3e26ee
unknown <amit.bhabua@gmail.com>
2020-07-09 03:12:02 +0400

    Week 1 Assignment Notebook

---
+++ Capstone_week1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+## This Notebook will mainly be used for capstone project
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
